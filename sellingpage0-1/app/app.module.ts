09e06628a276694086dd5cf6bda63e770515c3c9
rayjung95 <rayjung95@gmail.com>
2017-05-10 09:35:48 -0700

    added order status div

--- sellingpage0-1/app/app.module.ts
+++ sellingpage0-1/app/app.module.ts
@@ -1,17 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
+import { FoodItemComponent } from './food-item.component';
 import { SidebarComponent } from './sidebar.component';
+import { FoodItemService } from './food-item.service';
+import { FoodOrderListComponent } from './food-order-list.component';
+
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   declarations: [
     AppComponent,
-    MediaItemComponent,
-    SidebarComponent
+    FoodItemComponent,
+    SidebarComponent,
+    FoodOrderListComponent
+  ],
+  providers: [
+    FoodItemService
   ],
   bootstrap: [
     AppComponent
