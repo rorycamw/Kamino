453c124cfa7a719a8044e08770da21c322bedfd0
Rodney Tran <rodney.tran00@hotmail.com>
2017-05-24 14:47:09 -0700

    removed food-website folder

--- food-website/src/app/promotion-page/promotion-page.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component ({
-  selector:'promo-app',
-  templateUrl:'./promotion-page.component.html',
-  styleUrls:['./promotion-page.component.css',]
-})
-export class PromotionPageComponent {}
